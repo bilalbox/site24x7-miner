e469b80ffe3586db3b0c0e64499ff6a42d521f4f
Nasir Bilal <bilalbox@gmail.com>
2017-11-20 10:25:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# incapsula-miner
+MineMeld Miner for Incapsula public IPs implemented as an extension
+
+## How it works
+
+This Miner periodically checks Imperva's Incapsula API (https://my.incapsula.com/api/integration/v1/ips) for updates to the list of IP addresses associated with their WAF service.
+
+## Installation
+
+You can install this extension directly from the git repo
---
+++ incapsulaminer/node.py
@@ -0,0 +1,105 @@
+from __future__ import absolute_import
+
+import logging
+import requests
+import json
+from . import basepoller
+
+LOG = logging.getLogger(__name__)
+
+
+class IPv4(basepoller.BasePollerFT):
+    def configure(self):
+        super(IPv4, self).configure()
+
+        self.polling_timeout = self.config.get('polling_timeout', 20)
+        self.verify_cert = self.config.get('verify_cert', False)
+        self.url = 'https://my.incapsula.com/api/integration/v1/ips'
+
+    def _process_item(self, item):
+        # called on each item returned by _build_iterator
+        # it should return a list of (indicator, value) pairs
+        if item is None:
+            LOG.error('%s - no IP information found', self.name)
+            return []
+
+        else:
+            value = {
+                'type': 'IPv4',
+                'confidence': 100
+            }
+
+        return [[item, value]]
+
+    def _build_iterator(self, now):
+        # called at every polling interval
+        # here you should retrieve and return the list of items
+        rkwargs = dict(
+            stream=False,
+            verify=self.verify_cert,
+            timeout=self.polling_timeout,
+            data=[('resp_format','json'),]
+        )
+
+        r = requests.post(
+            self.url,
+            **rkwargs
+        )
+
+        try:
+            r.raise_for_status()
+        except:
+            LOG.debug('%s - exception in request: %s %s',
+                      self.name, r.status_code, r.content)
+            raise
+
+        # parse the results into a list
+        return iter(json.loads(r.text)['ipRanges'])
+
+class IPv6(basepoller.BasePollerFT):
+    def configure(self):
+        super(IPv6, self).configure()
+
+        self.polling_timeout = self.config.get('polling_timeout', 20)
+        self.verify_cert = self.config.get('verify_cert', False)
+        self.url = 'https://my.incapsula.com/api/integration/v1/ips'
+
+    def _process_item(self, item):
+        # called on each item returned by _build_iterator
+        # it should return a list of (indicator, value) pairs
+        if item is None:
+            LOG.error('%s - no IP information found', self.name)
+            return []
+
+        else:
+            value = {
+                'type': 'IPv6',
+                'confidence': 100
+            }
+
+        return [[item, value]]
+
+    def _build_iterator(self, now):
+        # called at every polling interval
+        # here you should retrieve and return the list of items
+        rkwargs = dict(
+            stream=False,
+            verify=self.verify_cert,
+            timeout=self.polling_timeout,
+            data=[('resp_format','json'),]
+        )
+
+        r = requests.post(
+            self.url,
+            **rkwargs
+        )
+
+        try:
+            r.raise_for_status()
+        except:
+            LOG.debug('%s - exception in request: %s %s',
+                      self.name, r.status_code, r.content)
+            raise
+
+        # parse the results into a list
+        return iter(json.loads(r.text)['ipv6Ranges'])
